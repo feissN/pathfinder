da6fe35e5e70f56962c276c2133737e6083ecde2
feissN <normanfeiss@web.de>
2021-06-28 18:02:54 +0200

    Removed unused menu bar

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Important! 
 // Open/Whitelist chanels in preload.js
 
-document.getElementById("close").addEventListener("click", e => {
-    window.api.send("close");
-});
+// document.getElementById("close").addEventListener("click", e => {
+//     window.api.send("close");
+// });
 
-document.getElementById("minimize").addEventListener("click", e => {
-    window.api.send("minimize");
-});
+// document.getElementById("minimize").addEventListener("click", e => {
+//     window.api.send("minimize");
+// });
