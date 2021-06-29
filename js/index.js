9e743192243b83110a5331550c950b5bb1b7f79a
feissN <86616781+feissN@users.noreply.github.com>
2021-06-29 14:56:53 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,2 @@
-// Important! 
-// Open/Whitelist chanels in preload.js
-
-// document.getElementById("close").addEventListener("click", e => {
-//     window.api.send("close");
-// });
-
-// document.getElementById("minimize").addEventListener("click", e => {
-//     window.api.send("minimize");
-// });
+// Disable context menu
+document.oncontextmenu = e => false;
